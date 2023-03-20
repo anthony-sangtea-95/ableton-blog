968338bdbeac07d99c9487aa212061ae57b85a5d
Anthony Sanga <sangatea2017@gmail.com>
2023-03-20 10:41:45 +0630

    fixed js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 let bar = document.querySelector(".bar");
 let left_links = document.querySelector(".nav-top__left-links");
 bar.onclick = function (){
-    console.log(this.src);
     left_links.classList.toggle("show");
-
-    if(this.src === "http://127.0.0.1:5501/images/bar.svg")
+    let url = window.location.origin;
+    if(this.src === `${url}/images/bar.svg`)
     {
-        this.src = "http://127.0.0.1:5501/images/cross.svg";
+        this.src = `${url}/images/cross.svg`;
     }
     else {
-        this.src = "http://127.0.0.1:5501/images/bar.svg";
+        this.src = `${url}/images/bar.svg`;
     }
 };
